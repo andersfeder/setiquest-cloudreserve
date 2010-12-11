7ec6d4a2a0eee430caa2ff272d9cea0e85f1eeba
Anders Feder <anders.feder.83@gmail.com>
2010-12-11 01:09:12 +0000

    Initial commit.

---
+++ api/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+// E-mail address of root administrator
+$cr_root_admin = "";
+
+// Database credentials
+$cr_db_host = "localhost";
+$cr_db_username = "cloudreserve";
+$cr_db_password = "";
+$cr_db_database = "cloudreserve";
+
+?>
---
+++ api/database.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once "config.inc.php";
+
+// Connecting, selecting database
+$cr_db_link = mysql_connect($cr_db_host, $cr_db_username, $cr_db_password)
+    or die('Could not connect: ' . mysql_error());
+mysql_select_db($cr_db_database)
+    or die('Could not select database');
+
+// Create tables if they don't already exist
+mysql_query("CREATE TABLE IF NOT EXISTS cr_requests (
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      owner VARCHAR(256),
+      time TIMESTAMP(8),
+      title VARCHAR(256),
+      description VARCHAR(512),
+      imageid VARCHAR(32),
+      mincount SMALLINT,
+      maxcount SMALLINT,
+      publickey VARCHAR(512),
+      instancetype VARCHAR(16),
+      begins DATETIME,
+      ends DATETIME
+    );");
+mysql_query("CREATE TABLE IF NOT EXISTS cr_approvals (
+      request_id INT NOT NULL PRIMARY KEY,
+      admin VARCHAR(256),
+      time TIMESTAMP(8),
+      FOREIGN KEY (request_id) REFERENCES cr_requests(id) ON DELETE CASCADE
+    );");
+?>
