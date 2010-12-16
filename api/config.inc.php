2111851e59e278107c3db5bfe39ffb8efdb27085
Anders Feder <anders.feder.83@gmail.com>
2010-12-16 02:55:25 +0000

    Added settings for mailer.

--- api/config.inc.php
+++ api/config.inc.php
@@ -12,8 +12,13 @@ define('CR_DB_USERNAME', 'cloudreserve');
 define('CR_DB_PASSWORD', '');
 define('CR_DB_DATABASE', 'cloudreserve');
 
+// Mailer settings
+define('CR_MAILER_URL', '')
+define('CR_MAILER_KEY', '')
+
 // E-mail settings
 define('CR_EMAIL_FROM', CR_ROOT_ADMIN);
+define('CR_EMAIL_REPLYTO', CR_ROOT_ADMIN);
 
 // Path: AWS SDK for PHP
 define('CR_PATH_AWSSDK', 'AWSSDKforPHP/');
